8a7491abb1db9be0f1d0d3374be496947cd6b6cf
Derek Morris <dmorris321@gmail.com>
2015-09-29 13:41:09 -0400

    greatly improve the accuracy of the panorama compiler .

--- panoramacompiler.c
+++ panoramacompiler.c
@@ -1,15 +1,19 @@
 #include "stupidvalve.h"
 
 #define SETOFFS( x , y )  x = ((char*)( y )) - ((char*)&( x ));
+#define setOffsetString(a,b) strncpy(pointer, (b), 64); (a) = pointer - ((char*)&(a)); pointer += strlen(b)+1;
 
-char* construct_file(char* sourcefilename, char* contentdirname, char* contents) {
+char* construct_file(char* sourcefilename, char* sourcefilename2, char* contentdirname, char* contents) {
 	uint32_t length = strlen(sourcefilename) + strlen(contents) + strlen(contentdirname) + 400;
 	svf_header* output = malloc(length);
+	char* pointer = (char*)output;
+	pointer += sizeof(svf_header);
 	output->always_twelve = 12;
 	output->version = 1;
 	output->lumps.offset = 8;
 	output->lumps.count = 2; // REDI and DATA
-	svflump_header* headers = (svflump_header*)(((char*)output) + sizeof(svf_header)); 
+	svflump_header* headers = (svflump_header*)(pointer); 
+	pointer += output->lumps.count*sizeof(svflump_header);
 	headers[0].tag[0]='R';
 	headers[0].tag[1]='E';
 	headers[0].tag[2]='D';
@@ -18,9 +22,10 @@ char* construct_file(char* sourcefilename, char* contentdirname, char* contents)
 	headers[1].tag[1]='A';
 	headers[1].tag[2]='T';
 	headers[1].tag[3]='A';
-	svfl_redi_header_datafile* srhd = (svfl_redi_header_datafile*)(((char*)headers) + output->lumps.count*sizeof(svflump_header));
+	svfl_redi_header_datafile* srhd = (svfl_redi_header_datafile*)(pointer);
+	pointer += sizeof(svfl_redi_header_datafile);
 	// we need this
-	srhd->sourceresource.count = 1;
+	srhd->sourceresource.count = 2;
 	srhd->sourceresourceadd.offset = 0;
 	srhd->sourceresourceadd.count = 0;
 	// we need this
@@ -39,44 +44,68 @@ char* construct_file(char* sourcefilename, char* contentdirname, char* contents)
 	srhd->extradata_float.count = 0;
 	srhd->extradata_string.offset = 0;
 	srhd->extradata_string.count = 0;
-	svfl_redi_sourceresource_datafile* sourceref = (svfl_redi_sourceresource_datafile*)(((char*)srhd) + sizeof(svfl_redi_header_datafile));
-	sourceref->CRC = 0; // Who cares about passing tests?
-	sourceref->flags = 2;
-	SETOFFS(srhd->sourceresource.offset, sourceref);
-	// we'll have the strings later
 
-	svfl_redi_argument_datafile* tdentry = (svfl_redi_argument_datafile*)(((char*)sourceref) + sizeof(svfl_redi_sourceresource_datafile));
+	// Information about source files
+	svfl_redi_sourceresource_datafile* sourceref = (svfl_redi_sourceresource_datafile*)pointer;
+	pointer += srhd->sourceresource.count * sizeof(svfl_redi_sourceresource_datafile);
+	sourceref[0].CRC = 0; // Who cares about passing tests?
+	sourceref[0].flags = 2;
+	sourceref[1].CRC = 0; // Who cares about passing tests?
+	sourceref[1].flags = 2;
+	SETOFFS(srhd->sourceresource.offset, sourceref);
+	setOffsetString(sourceref[0].offset_filename,sourcefilename);
+	setOffsetString(sourceref[0].offset_modname,contentdirname);
+	setOffsetString(sourceref[1].offset_filename,sourcefilename2);
+	setOffsetString(sourceref[1].offset_modname,contentdirname);
+
+	if((pointer - (char*)srhd)%4 != 0) {
+		pointer += 4-((pointer-(char*)srhd)%4);
+	}
+
+	// Creation arguments
+	svfl_redi_argument_datafile* tdentry = (svfl_redi_argument_datafile*)(pointer);
+	pointer += srhd->arguments.count * sizeof(svfl_redi_argument_datafile);
 	tdentry->fingerprint = 0;
 	tdentry-> fingerprint_default = 0;
 	SETOFFS(srhd->arguments.offset, tdentry);
-	// we'll have the strings later
+	setOffsetString(tdentry->offset_name,"___OverrideInputData___");
+	setOffsetString(tdentry->offset_type,"BinaryBlobArg");
 
-	svfl_redi_namemap_datafile* ndentry = (svfl_redi_namemap_datafile*)(((char*)tdentry) + sizeof(svfl_redi_argument_datafile));
+	if((pointer - (char*)srhd)%4 != 0) {
+		pointer += 4-((pointer-(char*)srhd)%4);
+	}
+
+	// Named data map
+	svfl_redi_namemap_datafile* ndentry = (svfl_redi_namemap_datafile*)pointer;
+	pointer += srhd->namemap.count*sizeof(svfl_redi_namemap_datafile);
 	ndentry->fingerprint = 5;
 	ndentry->userdata = 0;
 	SETOFFS(srhd->namemap.offset,ndentry);
-	//we'll have the strings later
+	setOffsetString(ndentry->offset_key,"CompilePanorama");
+	setOffsetString(ndentry->offset_expanded,"Panorama Compiler Version");
 
-	svfl_redi_extradata_int_datafile* sdentry = (svfl_redi_extradata_int_datafile*)(((char*)ndentry) + sizeof(svfl_redi_namemap_datafile));
+	if((pointer - (char*)srhd)%8 != 0) {
+		pointer += 8-((pointer-(char*)srhd)%8);
+	}
+
+	// Extra int data
+	svfl_redi_extradata_int_datafile* sdentry = (svfl_redi_extradata_int_datafile*)pointer;
+	pointer += srhd->extradata_int.count*sizeof(svfl_redi_extradata_int_datafile);
 	sdentry->value = 0;
 	SETOFFS(srhd->extradata_int.offset,sdentry);
-
-	//Ok, now for strings
-	char* pointer = (char*)(((char*)sdentry) + sizeof(svfl_redi_extradata_int_datafile));
-
-#define setOffsetString(a,b) strncpy(pointer, (b), 64); (a) = pointer - ((char*)&(a)); pointer += strlen(b)+1;
-
-	setOffsetString(sourceref->offset_filename,sourcefilename);
-	setOffsetString(sourceref->offset_modname,contentdirname);
-	setOffsetString(tdentry->offset_name,"__OverrideInputData");
-	setOffsetString(tdentry->offset_type,"BinaryBlobArg");
-	setOffsetString(ndentry->offset_expanded,"Panorama Compiler Version");
-	setOffsetString(ndentry->offset_key,"CompilePanorama");
 	setOffsetString(sdentry->offset_key,"IsChildResource");
+
+	// Close out redi by padding it to the next 16-byte alignment
+	/*if((pointer-(char*)srhd) % 16 != 0) {
+		pointer += 16-((pointer-(char*)srhd)%16);
+	}*/
 
 	// ok, that's it for REDI
 	SETOFFS(headers[0].offset,srhd);
 	headers[0].length = pointer - ((char*)srhd);
+	if((pointer - (char*)output)%16 != 0) {
+		pointer += 16-((pointer-(char*)output)%16);
+	}
 	SETOFFS(headers[1].offset,pointer);
 	headers[1].length = strlen(contents) + 6;
 	strncpy(pointer,"",6);
@@ -88,8 +117,8 @@ char* construct_file(char* sourcefilename, char* contentdirname, char* contents)
 }
 
 int main(int argc, char** argv) {
-	if(argc < 5) {
-		printf("Penguinwizzard Panorama Compiler v0.1\nUsage: ./panoramacompiler <filename> <outputfile> <internalfilename> <contentdirectory>\nThe last two are used inside the file for reference info.\n");
+	if(argc < 6) {
+		printf("Penguinwizzard Panorama Compiler v0.1\nUsage: ./panoramacompiler <filename> <outputfile> <internalfilename> <internalfilename2> <contentdirectory>\nThe last two are used inside the file for reference info.\n");
 		return 1;
 	}
 	FILE* f = fopen(argv[1],"r");
@@ -108,7 +137,11 @@ int main(int argc, char** argv) {
 	}
 	fclose(f);
 	buffer[size]='\0';
-	char* output = construct_file(argv[3],argv[4],buffer);
+	if(buffer[size-1] == '\n') {
+		size--;
+		buffer[size] = '\0';
+	}
+	char* output = construct_file(argv[3],argv[4],argv[5],buffer);
 	FILE* out = fopen(argv[2],"w");
 	if(out == NULL) {
 		printf("File open error; could not open output file; error code is %i\n",errno);
